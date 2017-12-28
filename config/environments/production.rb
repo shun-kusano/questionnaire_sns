05f4fcf220891933fbe3c2722eac5acea32167ca
草野 駿一 <KUSANOMAC@kusano-shunichi-no-MacBook-Pro.local>
2017-12-28 20:56:46 +0900

    “fix-mailer-url”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://tranquil-caverns-51725.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://tranquil-caverns-51725.herokuapp.com' }
   #config.action_mailer.default_url_options = { host: 'http://54.92.96.94/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
